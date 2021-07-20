da7172f116623c45059ee356192d2859752dc3e8
akira <akira1278@gmail.com>
2021-07-20 13:21:25 +0900

    計算のコード

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int sum(int, int);
+int main(){
+  // Ex-2:計算式を入力し，答えを出力するプログラム
+  int A, B;
+  string op;//文字列
+  //「3 * 4」のように，（整数）[スペース]（演算子）[スペース]（整数）の形で入力
+  cin >> A >> op >> B;
 
-int main()
-{
-    int a = 1; int b = 9;
-    std::cout<<" sum(a,b)"<<std::endl;
-    return 0;
-}
+  if (op == "+") {
+    cout << A + B << endl;
+  }else if (op == "-"){
+    cout << A - B << endl;
+  }else if (op == "*"){
+    cout << A * B << endl;
+  }else if (op == "/"){
+    cout << A / B << endl;
+  }
 
-int sum(int a,int b)
-{
-    return a+b;
+  return 0;
 }
